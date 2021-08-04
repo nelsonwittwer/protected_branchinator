e1502096296b8da76208fc848bec9c0ab038cfe7
Nelson Wittwer <nelsonwittwer@gmail.com>
2021-08-04 12:27:50 -0400

    terraform all the teh things

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -6,15 +6,17 @@ resource "aws_ecs_task_definition" "protected_branchinator" {
   family                   = "protected_branchinator"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "${var.fargate_cpu}"
-  memory                   = "${var.fargate_memory}"
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = <<DEFINITION
 [
   {
     "cpu": ${var.fargate_cpu},
     "memory": ${var.fargate_memory},
-    "image": "${var.container_image}",
+    "image": "${aws_ecr_repository.protected_branchinator.repository_url}",
     "name": "protected_branchinator",
     "networkMode": "awsvpc"
   }
@@ -24,14 +26,18 @@ DEFINITION
 
 resource "aws_ecs_service" "protected_branchinator" {
   name               = "protected_branchinator"
-  cluster            = "${aws_ecs_cluster.protected_branchinator.id}"
-  task_definition    = "${aws_ecs_task_definition.protected_branchinator.arn}"
-  desired_count      = "${var.container_count}"
+  cluster            = aws_ecs_cluster.protected_branchinator.id
+  task_definition    = aws_ecs_task_definition.protected_branchinator.arn
+  desired_count      = var.container_count
   launch_type        = "FARGATE"
 
   network_configuration {
-    subnets          = ["${aws_subnet.public.id}"]
+    subnets          = [data.aws_subnet.public.id]
     assign_public_ip = true
-    security_groups  = ["${aws_security_group.http_access.id}"]
+    security_groups  = [data.aws_security_group.http_access.id]
   }
+}
+
+resource "aws_ecr_repository" "protected_branchinator" {
+  name = "protected_branchinator"
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,14 +6,6 @@ data "aws_subnet" "public" {
   id = var.aws_public_subnet_id
 }
 
-data "aws_internet_gateway" "public" {
-  id = var.aws_internet_gateway_id
-}
-
-data "aws_route" "internet_access" {
-  id = var.aws_route_table_id
-}
-
 data "aws_security_group" "http_access" {
   id = var.aws_http_access_security_group_id
 }
