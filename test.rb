6695ea56898fc5e3c1129e4a67fabd52bad1af25
Nelson Wittwer <nelsonwittwer@gmail.com>
2021-08-09 09:32:57 -0400

    remove old test file

--- test.rb
+++
@@ -1,8 +0,0 @@
-require 'dotenv/load'
-require "pry-nav"
-require "logger"
-require_relative "./github"
-logger = ::Logger.new(STDERR) # FIXME - can't get STDOUT to work with ECS / cloudwatch out of the box
-logger.level = ::Logger::INFO
-
-GitHub.protect_repo("DevMixtape/testing123", "main", logger)
